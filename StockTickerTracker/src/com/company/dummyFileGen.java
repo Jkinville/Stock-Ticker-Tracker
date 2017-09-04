f4d065e966ca095b4b4c70c05650acf424c66a5c
John <John@DESKTOP-SRUGELD.hsd1.nh.comcast.net>
2017-09-03 23:38:36 -0400

    Testing

--- StockTickerTracker/src/com/company/dummyFileGen.java
+++ StockTickerTracker/src/com/company/dummyFileGen.java
@@ -2,43 +2,54 @@ package com.company;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class dummyFileGen {
 	File fileToZip;
-	
+	String dummyName;
 	public dummyFileGen(String fileToZipName, String dummyName) {
 		fileToZip = new File(fileToZipName);
-		
-		fileToZip.mkdir();
-		fillFile(dummyName);
+		this.dummyName = dummyName;
 		zipDir();
+		//fillFile(dummyName);
+		
 	}
 	
-	private void fillFile(String dummyName) {
+	private void fillFile(ZipOutputStream zip) {
 		//FileOutputStream newOutputStream = new FileOutputStream()
 		for(int i = 1; i < 1000; ++i) {
 			String fileName = dummyName + "_" + i;
-			File newFile = new File(fileToZip, fileName+".txt");
-			if(!newFile.exists()) {
-				try {
-					newFile.createNewFile();
-				}catch (IOException e) {};
+			ZipEntry newFile = new ZipEntry(fileName+".txt");
+			
+			try {
+				zip.putNextEntry(newFile);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			try {
+				zip.closeEntry();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		
 	}
 	private void zipDir() {
 		FileOutputStream dest = null;
 		try {
-			dest = new FileOutputStream(fileToZip + ".zip");
+			dest = new FileOutputStream(fileToZip);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ZipOutputStream zippedFi = new ZipOutputStream(new BufferedOutputStream(dest));
+		fillFile(zippedFi);
 		try {
 			dest.close();
 		} catch (IOException e) {
